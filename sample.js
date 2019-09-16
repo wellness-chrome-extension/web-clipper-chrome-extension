545762c03d1232e4434dfcdf67b68846a9d83379
Thomas Pollick <thomascpollick@gmail.com>
2019-09-16 12:31:02 -0500

    Added context menu and temporary icon

---
+++ sample.js
@@ -0,0 +1,16 @@
+function genericOnClick(info, tab) {
+    console.log("item " + info.menuItemId + " was clicked");
+    console.log("info: " + JSON.stringify(info));
+    console.log("tab: " + JSON.stringify(tab));
+  }
+  
+  // Create one test item for each context type.
+  var contexts = ["page","selection","link","editable","image","video",
+                  "audio"];
+  for (var i = 0; i < contexts.length; i++) {
+    var context = contexts[i];
+    var title = "Add '" + context + "' to your inspiration list";
+    var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                         "onclick": genericOnClick});
+    console.log("'" + context + "' item:" + id);
+  }
