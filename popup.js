d081432f5e59a8e882ccc44e653167c2052da2e1
Thomas Pollick <thomascpollick@gmail.com>
2019-09-20 22:16:02 -0500

    added event listener for chrome storage

--- popup.js
+++ popup.js
@@ -1,7 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     var link = document.getElementById('link');
+
     // onClick's logic below:
     link.addEventListener('click', function() {
         document.getElementById("link").style.fontWeight = 'bold';
     });
 });
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    console.log('new data type is %s. New value is %s', changes['type'].newValue, changes['data'].newValue)
+    for (var key in changes) {
+      var storageChange = changes[key];
+      console.log('Storage key "%s" in namespace "%s" changed. ' +
+                  'Old value was "%s", new value is "%s".',
+                  key,
+                  namespace,
+                  storageChange.oldValue,
+                  storageChange.newValue);
+    //document.getElementById('output').innerHTML = 'hey'
+
+    }
+  });
+
+// let val = 'hi'
+// document.addEventListener('oncontextmenu', function () {
+//     document.getElementById('output').innerHTML = val
+// });
+
+    // await chrome.storage.sync.get(['data'], function(result) {
+    //     val = result.data;
+    // })
