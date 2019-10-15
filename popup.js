c5ce750f8d449f1cd286b291bb64b14c75789081
Thomas Pollick <thomascpollick@gmail.com>
2019-10-15 13:44:33 -0500

    immediately add web clipping to popup HTML

--- popup.js
+++ popup.js
@@ -2,16 +2,16 @@
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    let val = 'hi'
+    let val
     let link1 = document.getElementById('link');
     chrome.storage.sync.get(['data'], function(result) {
       val = result.data;
+      document.getElementById('output').innerHTML = val;
     })
     // onClick's logic below:
     if (link1){
       link1.addEventListener('click', function() {
         document.getElementById("link").style.fontWeight = 'bold';
-        document.getElementById('output').innerHTML = val;
       });
       
     }
@@ -35,11 +35,5 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
 
   });
 
-
-
-// let val = 'hi'
-// document.addEventListener('oncontextmenu', function () {
-//     document.getElementById('output').innerHTML = val
-// });
 
 
